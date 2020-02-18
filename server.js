c553ca0e198efceb01d5ed211eabaa1a4b31441b
jwats057 <46846829+jwats057@users.noreply.github.com>
2020-02-18 14:41:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const http = require('http').createServer(app);
 const https = require('https');
 
 const fs = require('fs');
-const privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+const privateKey = fs.readFileSync('ssl/server.key', 'utf8');
 const certificate = fs.readFileSync('ssl/server.crt', 'utf8');
 
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
 const io = require('socket.io')(http);
 
@@ -24,7 +24,7 @@ const allowUrl = ['courses', 'modules'];
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 /*const corsOptions = {
     origin: '*',
     methods: ["POST", "GET"],
@@ -70,21 +70,21 @@ res.header('Access-Control-Allow-Headers', 'accept, Content-Type, Authorization,
 })*/
 
 app.get("/", (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World2');
 });
 
 app.get("/test", (req, res) => {
-    res.json({ test: "Hello test!"});
+    res.json({ test: "Hello test!" });
 })
 
 const sql = require("./app/models/db");
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('a user connected');
     socket.on('search', (data) => {
         console.log(data);
         sql.query(`SELECT * FROM courses WHERE name LIKE '${data}%'`, (err, res) => {
-            if(err)
+            if (err)
                 return
             io.emit('search-data', res);
         });
@@ -99,10 +99,10 @@ require("./app/routes/module.routes.js")(app);
 // [SH] Catch unauthorised errors
 app.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
-      res.status(401);
-      res.json({"message" : err.name + ": " + err.message});
+        res.status(401);
+        res.json({ "message": err.name + ": " + err.message });
     }
-  });
+});
 
 http.listen(port, () => {
     console.log("http Server is running on port: " + port);
