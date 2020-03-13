e021d11b4e1e5c60d940812c9b532d61a428685e
jwats057 <46846829+jwats057@users.noreply.github.com>
2020-03-13 02:20:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,128 +1,3 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-const cookieParser = require('cookie-parser');
-const keys = require('./app/config/keys');
-
-const app = express();
-const http = require('http').createServer(app);
-const https = require('https');
-
-const fs = require('fs');
-const privateKey = fs.readFileSync('ssl/server.key', 'utf8');
-const certificate = fs.readFileSync('ssl/server.crt', 'utf8');
-
-const credentials = { key: privateKey, cert: certificate };
-const httpsServer = https.createServer(credentials, app);
-const io = require('socket.io')(http);
-
-const port = process.env.PORT || 3000;
-
-const allowUrl = ['courses', 'modules'];
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-/*const corsOptions = {
-    origin: '*',
-    methods: ["POST", "GET"],
-    credentials: true,
-    maxAge: 3600
-};*/
-app.use(cors());
-
-// Auth 
-app.use(cookieSession({
-    name: 'LMS-FIU-session',
-    maxAge: 24 * 60 * 60 * 1000,
-    keys: [keys.session.cookieKey]
-}));
-
-app.use(cookieParser());
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-const passportSetup = require('./app/config/passport-setup');
-const authRoutes = require('./app/routes/auth-routes');
-const profileRoutes = require('./app/routes/profile-routes');
-
-/*const authenticationMiddleware = (req, res, next) => {
-
-    if (passport.authenticate('facebook')) {
-        console.log("Authorized access");
-        return next()
-    }
-    res.redirect('/');
-}*/
-
-//app.use(authenticationMiddleware);
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-
-/*app.all(() => {
-res.header('Access-Control-Allow-Origin', '*'); // your website
-//res.header('Access-Control-Allow-Credentials', 'false');
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-res.header('Access-Control-Allow-Headers', 'accept, Content-Type, Authorization, Content-Length, X-Requested-With')
-})*/
-
-app.get("/", (req, res) => {
-    res.send('Hello World4');
-});
-
-app.get("/test", (req, res) => {
-    res.json({ test: "Hello test!" });
-})
-
-const sql = require("./app/models/db");
-
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('search', (data) => {
-        console.log(data);
-        sql.query(`SELECT * FROM courses WHERE name LIKE '${data}%'`, (err, res) => {
-            if (err)
-                return
-            io.emit('search-data', res);
-        });
-    })
-})
-
-require("./app/routes/student.routes.js")(app);
-require("./app/routes/course.routes.js")(app);
-require("./app/routes/student-course.routes.js")(app);
-require("./app/routes/module.routes.js")(app);
-require("./app/routes/video.routes.js")(app);
-
-// [SH] Catch unauthorised errors
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401);
-        res.json({ "message": err.name + ": " + err.message });
-    }
-});
-
-
-/* Facebook tab request handling */
-
-
-app.post('/', function (req, res) {
-    res.send("Post request sent to backend");
-});
-
-
-http.listen(port, () => {
-    console.log("http Server is running on port: " + port);
-});
-/*
-httpsServer.listen(port, () => {
-    console.log("https Server is running on port: " + port);
-})
-*/
-=======
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -219,7 +94,7 @@ const data = readPdfFile(inputfile);
 
 app.post("/testpdf", upload.any(), (req, res) => {
     console.log("testpdf posted");
-    if(req.files){
+    if (req.files) {
         const file = req.files;
         console.log(file[0].buffer);
         console.log("module_id: " + req.body.fileKey);
@@ -236,8 +111,8 @@ app.post("/testpdf", upload.any(), (req, res) => {
 
 app.get("/testpdf", (req, res) => {
     sql.query("SELECT * FROM pdfs", (err, res) => {
-        if(err) throw err;
-        const row = res[res.length-1];
+        if (err) throw err;
+        const row = res[res.length - 1];
         const data = row.pdf;
         console.log("BLOB data read! " + data);
         const buf = new Buffer.from(data, "binary");
@@ -249,7 +124,7 @@ app.get("/testpdf", (req, res) => {
 const sql = require("./app/models/db");
 
 io.on('connection', function (socket) {
-    console.log('a user connected: '+ socket.id);
+    console.log('a user connected: ' + socket.id);
     socket.on('search', (data) => {
         console.log(data);
         sql.query(`SELECT * FROM courses WHERE name LIKE '${data}%'`, (err, res) => {
