ca8ee32015c8178facc200fa1dc160f7838202b8
jwats057 <46846829+jwats057@users.noreply.github.com>
2020-03-30 03:19:41 -0400

    Finished testing, cleanup

--- app/routes/student-course.routes.js
+++ app/routes/student-course.routes.js
@@ -3,17 +3,17 @@ module.exports = app => {
 
     app.post("/student-courses", studentcourse.create);
 
-    app.get("/student-courses", studentcourse.findAll); // Verified
+    app.get("/student-courses", studentcourse.findAll);
 
-    app.get("/student-courses/s/:studentEmail", studentcourse.findOneStudent); // 
+    app.get("/student-courses/s/:studentEmail", studentcourse.findOneStudent);
 
-    app.get("/student-courses/c/:courseId", studentcourse.findOneCourse); //
+    app.get("/student-courses/c/:courseId", studentcourse.findOneCourse);
 
-    app.get("/student-courses/p/:courseId/:studentEmail", studentcourse.getAvgPts);//
+    app.get("/student-courses/p/:courseId/:studentEmail", studentcourse.getAvgPts);
 
-    app.put("/student-courses/:studentEmail", studentcourse.update);
+    app.put("/student-courses/:studentEmail", studentcourse.update); // Only updates enrollment status
 
     app.delete("/student-courses/:studentEmail/:courseId", studentcourse.delete);
 
-    app.delete("/student-courses", studentcourse.deleteAll); 
+    app.delete("/student-courses", studentcourse.deleteAll);
 };
